29f070fbc8627e6ea0f1ac5f34849c65e1e4a2aa
Dalai Felinto <dfelinto@gmail.com>
2016-11-28 20:43:07 +0100

    Francesco awesome update

--- workflow_shot_tool/operators.py
+++ workflow_shot_tool/operators.py
@@ -137,7 +137,7 @@ class ST_SetMetadataOperator(Operator):
 
 
         if shot_type == SHOT_TYPE.LAYOUT:
-            render.use_stamp = False
+            render.use_stamp = True
 
         elif shot_type == SHOT_TYPE.ANIMATION:
             render.use_stamp = True
@@ -152,7 +152,7 @@ class ST_SetMetadataOperator(Operator):
             render.use_stamp_render_time = False
 
         elif shot_type == SHOT_TYPE.LIGHTING:
-            render.use_stamp = True
+            render.use_stamp = False
             render.use_stamp_render_time = True
             render.use_stamp_memory = True
 
